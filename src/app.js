dd4d0e0f4496b64ddbe1be6fe2370dc5b8a76378
Patricia Alvarez <168550797+PatriciaAlEs@users.noreply.github.com>
2025-07-11 17:23:25 +0000

    tarjeta de personaje

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-import "bootstrap";
-import "./style.css";
+let contador = 0;
+const colores = ["#60935D", "#FF579F", "#FFAA00", "#00BFFF", "#8A2BE2"];
+
+function mostrarMensaje() {
+  contador++;
+
+  let mensaje = "";
+
+  if (contador === 1) {
+    mensaje = "Los Nargles se esconden cuando los buscas 👀";
+  } else if (contador === 2) {
+    mensaje = "¿Sabías que el Sombrero Seleccionador casi la puso en Gryffindor?";
+  } else if (contador < 5) {
+    mensaje = `Curiosidad mágica número ${contador} desbloqueada ✨`;
+  } else if (contador === 5) {
+    mensaje = "¡Luna puede ver a los Thestrals porque vio morir a su madre! 🐎";
+  } else if (contador < 10) {
+    mensaje = `¡Wow, ya hiciste ${contador} clics mágicos! 🪄`;
+  } else if (contador === 10) {
+  mensaje = "Ya no quedan más secretos por revelar... o sí 😏";
+  alert("¡Eh! 🧙‍♀️ Ya hiciste suficiente magia... la página se recargará.");
+  location.reload();
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  document.getElementById("mensaje").textContent = "Hello";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const btn = document.querySelector("button");
+  const colorRandom = colores[Math.floor(Math.random() * colores.length)];
+  btn.style.backgroundColor = colorRandom;
+}
+
+console.log("Hola que ase");
